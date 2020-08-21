7b388d14657dd3abfa820bc9a89549c7f5dd4d38
Phenix Rizen <phenixrizen@gmail.com>
2020-08-21 15:18:03 -0500

    first commit

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+# Update packages
+sudo apt-get update
+
+# Install Git
+sudo apt-get install git -y
+
+# Git Katran
+git clone git@github.com:facebookincubator/katran.git
+
+# Install Katran Deps
+cd katran
+./getdeps.sh 
+
+# Build Katran
+./build_katran.sh
+
+# Show message about successfully install
+echo "Everything done, have a nice day!"
